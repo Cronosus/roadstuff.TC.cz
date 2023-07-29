483d52575ae82f7e75b2115f336184cec8016464
CronosusCZ <cronosus@hotmail.com>
2023-07-29 20:37:49 +0200

    Update Reference.java

--- src/main/java/net/killermapper/roadstuff/common/Reference.java
+++ src/main/java/net/killermapper/roadstuff/common/Reference.java
@@ -1,15 +1,20 @@
 package net.killermapper.roadstuff.common;
-
 public class Reference
 {
     //The number of each different signs, for each shape
-    
-    public static final short maxSignDiamond = 116;
-    public static final short maxSignSquare = 104;
-    public static final short maxSignCircle = 113;
-    public static final short maxSignTriangle = 96;
-    public static final short maxSignRectangle = 85;
+
+    public static final short maxSignDiamond = 243;
+    public static final short maxSignSquare = 209;
+    public static final short maxSignCircle = 140;
+    public static final short maxSignTriangle = 105;
+    public static final short maxSignRectangle = 138;
     public static final short maxSignMisc = 76;
-    public static final short maxSignMiscB = 9;
-    public static final String version = "1.0.3-final";
+    public static final short maxSignMiscB = 7;
+    public static final short maxSignLetter = 145;
+    public static final short maxSignLetterB = 3;
+    public static final short maxSignTrain = 80;
+    public static final short maxSignTrainB = 13;
+    public static final short maxSignSpeed = 137;
+    public static final short maxSignSpeedB = 3;
+    public static final String version = "1.0.3a-TC.cz-custom";
 }
